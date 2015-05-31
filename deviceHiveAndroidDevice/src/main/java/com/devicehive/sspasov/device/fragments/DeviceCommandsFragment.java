af694b8a0cdb8da5ba8ed977f7a88f98d341521f
Stanimir Spaov <sspasov92@gmail.com>
2015-05-31 14:44:45 +0300

    [layout] reworked commands fragment

--- deviceHiveAndroidDevice/src/main/java/com/devicehive/sspasov/device/fragments/DeviceCommandsFragment.java
+++ deviceHiveAndroidDevice/src/main/java/com/devicehive/sspasov/device/fragments/DeviceCommandsFragment.java
@@ -1,47 +1,118 @@
 package com.devicehive.sspasov.device.fragments;
 
-import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import com.actionbarsherlock.app.SherlockFragment;
 import com.dataart.android.devicehive.Command;
 import com.devicehive.sspasov.device.R;
 
 import java.util.List;
 
-public class DeviceCommandsFragment extends SherlockFragment {
+public class DeviceCommandsFragment extends ListFragment {
 
-	private List<Command> commands;
+    private final String TAG = DeviceCommandsFragment.class.getSimpleName();
 
-	private TextView logTextView;
+    private List<Command> commands;
+    private CommandsAdapter commandsAdapter;
 
-	public void setCommands(List<Command> commands) {
-		this.commands = commands;
-		if (isAdded()) {
-			setupCommands(commands);
-		}
-	}
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_device_commands, container, false);
+    public static DeviceCommandsFragment newInstance() {
+        return new DeviceCommandsFragment();
+    }
 
-		logTextView = (TextView) view.findViewById(R.id.log_text_view);
-		setupCommands(commands);
-		return view;
-	}
+    public void setCommands(List<Command> commands) {
+        this.commands = commands;
+        if (getActivity() != null) {
+            commandsAdapter = new CommandsAdapter(getActivity(), commands);
+            commandsAdapter.notifyDataSetChanged();
+            setListAdapter(commandsAdapter);
+        }
+    }
 
-	private void setupCommands(List<Command> commands) {
-		if (commands != null) {
-			logTextView.setText("");
-			for (Command command : commands) {
-				logTextView.append(command.toString() + "\n\n");
-			}
-		}
-	}
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        if (commands != null) {
+            commandsAdapter = new CommandsAdapter(activity, commands);
+            setListAdapter(commandsAdapter);
+        }
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(commands.get(position).toString());
+        Dialog dialog = builder.create();
+        dialog.setCancelable(true);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.show();
+    }
+
+    private static class CommandsAdapter extends BaseAdapter {
+
+        private final LayoutInflater inflater;
+        private List<Command> commands;
+
+        public CommandsAdapter(Context context, List<Command> commands) {
+            this.commands = commands;
+            this.inflater = LayoutInflater.from(context);
+        }
+
+        public void setCommands(List<Command> commands) {
+            this.commands = commands;
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return commands.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return commands.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.command_list_item, null);
+                holder = new ViewHolder();
+                holder.name = (TextView) convertView.findViewById(R.id.tv_command_name);
+                holder.params = (TextView) convertView.findViewById(R.id.tv_command_params);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            final Command command = commands.get(position);
+            holder.name.setText(command.getId() + ". " + command.getCommand());
+            holder.params.setText(command.getParameters().toString());
+
+            return convertView;
+        }
+
+        private class ViewHolder {
+            TextView name;
+            TextView params;
+        }
+
+    }
 
 }
